da9f2d00c5c34b2a9b12ae775c5a7b43523338ab
Chandima Ranaweera <chandima@bistecglobal.com>
2022-10-26 08:23:29 +0530

    implement truffle script

--- truffle/scripts/increment.js
+++ truffle/scripts/increment.js
@@ -5,19 +5,19 @@
   https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts
 */
 
-const SimpleStorage = artifacts.require("SimpleStorage");
+const Adoption = artifacts.require("Adoption");
 
 module.exports = async function (callback) {
-  const deployed = await SimpleStorage.deployed();
+  const deployed = await Adoption.deployed();
 
-  const currentValue = (await deployed.read()).toNumber();
-  console.log(`Current SimpleStorage value: ${currentValue}`);
+  const currentValue = await deployed.getAdopters();
+  console.log(`Current Adoption value: ${currentValue[1]}`);
 
-  const { tx } = await deployed.write(currentValue + 1);
+  const { tx } = await deployed.adopt(1);
   console.log(`Confirmed transaction ${tx}`);
 
-  const updatedValue = (await deployed.read()).toNumber();
-  console.log(`Updated SimpleStorage value: ${updatedValue}`);
+  const updatedValue = await deployed.getAdopters();
+  console.log(`Updated Adoption value: ${updatedValue[1]}`);
 
   callback();
 };
